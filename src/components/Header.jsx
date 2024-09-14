a3c665e574b05ca0504706f6cedf2277f7fc2e8c
Rolando Duarte <rolandoduarte83@gmail.com>
2024-09-14 18:48:28 -0300

    estilizando logo y componentes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import logo from '../assets/logo.webp';
 
 const Header = () => {
     return (
         <header className="bg-blue-500 text-white p-4 text-center">
-            <h1 className="text-2xl font-bold">Lista de Compras</h1>
+            <img src={logo} alt="Logo" className="mx-auto rounded-lg mb-4 w-24 h-24" />
+            <h1 className="text-2xl font-bold">shopping list</h1>
         </header>
     );
 };
